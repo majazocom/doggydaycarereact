9ef332b94d12ec1f2eda78d1f1b807116cf33082
majazocom <maja.gedda@zocom.se>
2024-03-13 12:03:44 +0100

    added a function as a prop

--- src/Components/AllDogs.jsx
+++ src/Components/AllDogs.jsx
@@ -2,11 +2,18 @@ import SmallDogCard from "./SmallDogCard";
 
 export default function AllDogs(props) {
     const allDogs = props.dogs;
+    function handleClickedDog() {
+        console.log('clicked on a dog');
+    }
     return (
         <section className="allDogsContainer">
             <h2>All the dogs</h2>
             <section className="allDogsCards">
-                {allDogs.map((dog, i) => <SmallDogCard dog={dog} key={i} />)}
+                {
+                    allDogs.map((dog, i) =>
+                        <SmallDogCard dog={dog} key={i} handleClick={() => handleClickedDog()} />
+                    )
+                }
             </section>
         </section>
     )
