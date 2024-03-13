52b039023eaf2492af2c95d0fd1a5dd927a6acea
majazocom <maja.gedda@zocom.se>
2024-03-13 11:51:46 +0100

    added filter

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import AllDogs from './Components/AllDogs';
 
 function App() {
   const [allDogs, setAllDogs] = useState([]);
-  
+  const [presentDogs, setPresentDogs] = useState([]);
+  const [showAllDogs, setShowAllDogs] = useState(true);
+
   // hämta BARA från API:et när App.jsx först renderas mha useEffect
   useEffect(() => {
     // få in alla hundar från API:et
@@ -16,12 +18,29 @@ function App() {
     fetchDogs();
   }, []);
 
+  // funktion som hanterar när vi ändrar värde på vår select
+  function handleFilter(value) {
+    if (value === "Present dogs") {
+      // filtrera ut de hundar som är närvarande från vårt allDogs-state
+      let unfilteredDogs = [...allDogs];
+      let filteredDogs = unfilteredDogs.filter((dog) => dog.present === true);
+      // uppdaterar statet med våra närvarande hundar
+      setPresentDogs(filteredDogs);
+      // uppdaterar vi statet där vi bestämmer om vi ska se alla hundar eller bara de närvarande
+      setShowAllDogs(false);
+    } else {
+      setShowAllDogs(true);
+    }
+  }
 
   return (
     <>
       <h1>DOGGY DAYCARE</h1>
-      <input type='text' defaultValue="SÖKTEXT" />
-      <AllDogs dogs={allDogs} />
+      <select onChange={((event) => handleFilter(event.target.value))}>
+        <option>All dogs</option>
+        <option>Present dogs</option>
+      </select>
+      {showAllDogs ? <AllDogs dogs={allDogs} /> : <AllDogs dogs={presentDogs} />}
     </>
   )
 }
